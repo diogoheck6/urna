81bad3213748fe37899d02e7c319f56d61098de2
diogoheck6 <diogoheck6@gmail.com>
2025-11-22 10:11:50 -0300

    tela login

--- apps/frontend/src/app/(externa)/auth/page.tsx
+++ apps/frontend/src/app/(externa)/auth/page.tsx
@@ -1,3 +1,19 @@
+"use client"
 export default function TelaLogin() {
-	return <div>Tela de Login</div>
+	return (
+		<div className="flex flex-col justify-center items-center gap-6 h-screen">
+			<h1 className="text-3xl font-bold">Seja Bem Vindo(a)</h1>
+			<div className="flex flex-col gap-6 bg-zinc-900 rounded-lg w-96 p-7">
+				<div className="flex flex-col gap-1">
+					<span>E-mail</span>
+					<input type="text" value={""} className="input" onChange={(e) => {}} />
+				</div>
+				<div className="flex flex-col gap-1">
+					<span>Senha</span>
+					<input type="password" value={""} className="input" onChange={(e) => {}} />
+				</div>
+				<button className="botao azul">Entrar</button>
+			</div>
+		</div>
+	)
 }
